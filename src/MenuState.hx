b80c2b2e29d081b52e4769e190fa072b19603a30
Mikhail Cheshkov <muuuuuu@mail.ru>
2013-12-17 04:02:59 +0700

    Mixed tiles tileset

--- src/MenuState.hx
+++ src/MenuState.hx
@@ -1,5 +1,8 @@
 package;
 
+import flash.geom.Point;
+import flash.geom.Rectangle;
+import flash.display.BitmapData;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
@@ -14,6 +17,149 @@ import flixel.util.FlxMath;
  */
 class MenuState extends FlxState
 {
+    public function _mixV(i:Int,j:Int):Float{
+        return i/31;
+    }
+
+    public function _mixH(i:Int,j:Int):Float{
+        return j/31;
+    }
+
+    public function _mixDR(i:Int,j:Int):Float{
+        return Math.max(i,j)/31;
+    }
+
+    public function _mixDL(i:Int,j:Int):Float{
+        return Math.max((31-i),j)/31;
+    }
+
+    public function _mixUR(i:Int,j:Int):Float{
+        return Math.max(i,(31-j))/31;
+    }
+
+    public function _mixUL(i:Int,j:Int):Float{
+        return Math.max((31-i),(31-j))/31;
+    }
+
+    public function mix(bd1:BitmapData,bd2:BitmapData,mf:Int->Int->Float):BitmapData{
+        var bdmix:BitmapData = new BitmapData(32,32);
+
+        for (i in 0...32){
+            for (j in 0...32){
+                var p1 = bd1.getPixel(i,j);
+                var p2 = bd2.getPixel(i,j);
+
+                var r1:Float = (p1 >> 16 & 0xFF) / 255;
+                var g1:Float = (p1 >> 8  & 0xFF) / 255;
+                var b1:Float = (p1       & 0xFF) / 255;
+
+                var r2:Float = (p2 >> 16 & 0xFF) / 255;
+                var g2:Float = (p2 >> 8  & 0xFF) / 255;
+                var b2:Float = (p2       & 0xFF) / 255;
+
+//                var a1:Float = Math.sqrt(r1*r1 + g1 * g1 + b1*b1);
+//                var a2:Float = Math.sqrt(r2*r2 + g2 * g2 + b2*b2);
+
+                var a1:Float = Math.max(r1, Math.max(g1 , b1));
+                var a2:Float = Math.max(r2, Math.max(g2 , b2));
+
+//                a1 /= Math.max(a1,a2);
+//                a2 /= Math.max(a1,a2);
+
+//                var a1:Float = b1;
+//                var a2:Float = b2;
+
+                var mix:Float = mf(i,j);
+
+//                if (i < 8) mix = 0
+//                else if (i<24) mix = (i-8) / 15;
+//                else mix = 1;
+
+                var depth:Float = 0.2;
+                var ma:Float = Math.max(a1 + mix, a2 + (1-mix)) - depth;
+
+                var bb1:Float = Math.max(a1 + mix - ma, 0);
+                var bb2:Float = Math.max(a2 + (1-mix) - ma, 0);
+
+                var r = (Math.floor((r1 * bb1 + r2 * bb2) / (bb1 + bb2) * 255)) & 0xFF;
+                var g = (Math.floor((g1 * bb1 + g2 * bb2) / (bb1 + bb2) * 255)) & 0xFF;
+                var b = (Math.floor((b1 * bb1 + b2 * bb2) / (bb1 + bb2) * 255)) & 0xFF;
+
+                var pp = (r << 16) | (g << 8) | (b);
+
+                bdmix.setPixel(i,j,pp);
+            }
+        }
+
+        return bdmix;
+    }
+
+    public function makeSq(bdres:BitmapData,bd1:BitmapData,bd2:BitmapData){
+        var ul = mix(bd1,bd2,_mixUL);
+        var u  = mix(bd2,bd1,_mixH);
+        var ur = mix(bd1,bd2,_mixUR);
+        var l  = mix(bd2,bd1,_mixV);
+        //////////////
+        var r  = mix(bd1,bd2,_mixV);
+        var dl = mix(bd1,bd2,_mixDL);
+        var d  = mix(bd1,bd2,_mixH);
+        var dr = mix(bd1,bd2,_mixDR);
+
+        bdres.copyPixels(ul,ul.rect,new Point(0, h));
+        bdres.copyPixels(u, u.rect,new Point(32,h));
+        bdres.copyPixels(ur,ur.rect,new Point(64,h));
+        bdres.copyPixels(l, l.rect,new Point(0, h+32));
+        ///
+        bdres.copyPixels(r, r.rect,new Point(64,h+32));
+        bdres.copyPixels(dl,dl.rect,new Point(0, h+64));
+        bdres.copyPixels(d, d.rect,new Point(32,h+64));
+        bdres.copyPixels(dr,dr.rect,new Point(64,h+64));
+
+        h += 32 * 3;
+    }
+
+    public var h:Int;
+
+    public function mixing(){
+        var bd:BitmapData = Assets.getBitmapData("assets/tiled/tileset.png");
+
+        var arr:Array<{i:Int,j:Int}> = [];
+        arr.push({i:1,j:0});
+        arr.push({i:1,j:1});
+        arr.push({i:2,j:1});
+        arr.push({i:2,j:2});
+
+        var addRowCount = arr.length * (arr.length - 1) * 3; // because of 3x3 square
+
+        var bdres:BitmapData = new BitmapData(bd.width,bd.height + addRowCount * 32);
+        bdres.copyPixels(bd,bd.rect,new Point(0,0));
+        h=bd.height;
+
+        for (t1 in 0...arr.length){
+            for (t2 in t1+1...arr.length){
+                var bd1:BitmapData = new BitmapData(32,32);
+                var bd2:BitmapData = new BitmapData(32,32);
+
+                var i1 = arr[t1].i;
+                var j1 = arr[t1].j;
+                var i2 = arr[t2].i;
+                var j2 = arr[t2].j;
+
+                bd1.copyPixels(bd,new Rectangle(i1*32,j1*32,32,32),new Point(0,0));
+                bd2.copyPixels(bd,new Rectangle(i2*32,j2*32,32,32),new Point(0,0));
+
+                makeSq(bdres,bd1,bd2);
+                makeSq(bdres,bd2,bd1);
+            }
+        }
+
+        var ss:FlxSprite = new FlxSprite();
+        ss.loadGraphic(bdres);
+        ss.x = 10;
+        ss.y = -800;
+        add(ss);
+    }
+
 	/**
 	 * Function that is called up when to state is created to set it up. 
 	 */
@@ -29,6 +175,9 @@ class MenuState extends FlxState
         var a:FlxSprite = new FlxSprite();
         a.loadGraphic(Assets.getBitmapData("assets/images/menu.png"));
         add(a);
+
+
+//        mixing();
 
 		super.create();
 	}
